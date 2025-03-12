faa279d357f919553758e31e971167f94d9f521a
98donggyu <98donggyu@gmail.com>
2025-03-12 17:00:02 +0900

    요청 이미지를 json으로 직접 받아서 처리

--- 2-1.dalle.py
+++ 2-1.dalle.py
@@ -4,24 +4,28 @@ from dotenv import load_dotenv
 import os
 import requests
 from PIL import Image
-from io import BytesIO
+import base64
+import io
 
 load_dotenv(override=True)
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 
-# 이미지 url을 받음
+# 실제 이미지 값을 json값으로 리턴 받음.
 def get_image(prompt):
     response = client.images.generate(
         model="dall-e-3",
         prompt=prompt,
         size="1024x1024",
         quality="standard",
+        response_format='b64_json',
         n=1       
     )
-    image_url = response.data[0].url
-    return image_url
 
+    response = response.data[0].b64_json # DALLE로부터 Base64 형태의 이미지를 얻음.
+    image_data = base64.b64decode(response) # Base64로 쓰여진 데이터를 이미지 형태로 변환
+    image = Image.open(io.BytesIO(image_data)) # '파일처럼' 만들어진 이미지 데이터를 컴퓨터에서 볼 수 있도록 Open
+    return image
 
 
 st.title("그림 그리는 AI 화가 서비스 🧑‍‍🎨")
@@ -33,8 +37,8 @@ prompt = st.text_area("원하는 이미지의 설명을 영어로 적어보세�
 if st.button("Painting"):
     if prompt.strip():
         try:
-            image_url = get_image(prompt)
-            st.image(image_url, width=300)
+            image = get_image(prompt)
+            st.image(image, width=300)
         
         except Exception as e:
             st.error(f"이미지 생성 중 오류 발생: {e}")
--- README.md
+++ README.md
@@ -6,3 +6,11 @@ streamlit 실습
 pip install streamlit
 pip install openai
 ```
+
+# openai에서 이미지를 직접 리턴받을 때 필요
+```
+pip install Pillow
+```
+
+## 실행 결과
+<img src = "images/default_view.png", alt="초기 화면">
